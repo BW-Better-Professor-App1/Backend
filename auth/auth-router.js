3c5d7aab3ed04c3c65bb7f2506e8def20c1ff0f0
Phong Yang <phongyang88@hotmail.com>
2020-03-01 20:16:49 -0600

    added additional info to be returned when a user logs in or registers

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,6 +23,9 @@ router.post("/register", validateRegisterBody, validateUniqueEmail, (req, res) =
       const token = generateToken(newUser)
       res.status(201).json({
         message: "Successfully created a new user.",
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
         id: newUser.id,
         token
       })
@@ -56,6 +59,9 @@ router.post("/login", validateLoginBody, (req, res) => {
         const token = generateToken(user)
         res.status(200).json({
           message: `Welcome back, ${user.firstName}!`,
+          firstName: newUser.firstName,
+          lastName: newUser.lastName,
+          email: newUser.email,
           id: user.id,
           token
         })
