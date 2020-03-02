c01f787a5666a3b36892d077bc8a690af99ca24e
Jeffrey <jeffreymartespolanco@hotmail.com>
2020-03-02 12:33:56 -0800

    added project router and modal

--- auth/validate_middleware.js
+++ auth/validate_middleware.js
@@ -1,167 +1,179 @@
-const users = require('../users//users-model')
-const students = require('../students/students-model')
+const users = require("../users//users-model");
+const students = require("../students/students-model");
 
 module.exports = {
-    validateId,
-    validateRegisterBody,
-    validateUpdateBody,
-    validateLoginBody,
-    validateUniqueEmail,
-    validateUniqueStudentEmail,
-    validateStudentBody,
-    validateStudentId,
-}
+  validateId,
+  validateRegisterBody,
+  validateUpdateBody,
+  validateLoginBody,
+  validateUniqueEmail,
+  validateUniqueStudentEmail,
+  validateStudentBody,
+  validateStudentId,
+  validateProjectBody
+};
 
 function validateId(req, res, next) {
-    users.findBy({
-            id: req.params.id
-        })
-        .then(user => {
-            if (user) {
-                req.user = user;
-                next();
-            } else {
-                res.status(400).json({
-                    error: "That user does not exist."
-                })
-            }
-
-        })
-        .catch(({
-            name,
-            code,
-            message,
-            stack
-        }) => {
-            res.status(500).json({
-                name,
-                code,
-                message,
-                stack
-            })
-        })
+  users
+    .findBy({
+      id: req.params.id
+    })
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({
+          error: "That user does not exist."
+        });
+      }
+    })
+    .catch(({ name, code, message, stack }) => {
+      res.status(500).json({
+        name,
+        code,
+        message,
+        stack
+      });
+    });
 }
 
 function validateRegisterBody(req, res, next) {
-    Object.keys(req.body).length === 0 && req.body.constructor === Object ?
-        res.status(400).json({
-            error: "Missing request body."
-        }) :
-        !req.body.firstName || !req.body.lastName || !req.body.email || !req.body.password ?
-        res.status(400).json({
-            error: "firstName, lastName, email, and password are required."
-        }) :
-        next()
+  Object.keys(req.body).length === 0 && req.body.constructor === Object
+    ? res.status(400).json({
+        error: "Missing request body."
+      })
+    : !req.body.firstName ||
+      !req.body.lastName ||
+      !req.body.email ||
+      !req.body.password
+    ? res.status(400).json({
+        error: "firstName, lastName, email, and password are required."
+      })
+    : next();
 }
 
 function validateUpdateBody(req, res, next) {
-    Object.keys(req.body).length === 0 && req.body.constructor === Object ?
-        res.status(400).json({
-            error: "Missing request body."
-        }) :
-        !req.body.firstName || !req.body.lastName || !req.body.email ?
-        res.status(400).json({
-            error: "firstName, lastName, and email are required."
-        }) :
-        next()
+  Object.keys(req.body).length === 0 && req.body.constructor === Object
+    ? res.status(400).json({
+        error: "Missing request body."
+      })
+    : !req.body.firstName || !req.body.lastName || !req.body.email
+    ? res.status(400).json({
+        error: "firstName, lastName, and email are required."
+      })
+    : next();
 }
 
 function validateLoginBody(req, res, next) {
-    Object.keys(req.body).length === 0 && req.body.constructor === Object ?
-        res.status(400).json({
-            error: "Missing request body."
-        }) :
-        !req.body.email || !req.body.password ?
-        res.status(400).json({
-            error: "email and password are required"
-        }) :
-        next()
+  Object.keys(req.body).length === 0 && req.body.constructor === Object
+    ? res.status(400).json({
+        error: "Missing request body."
+      })
+    : !req.body.email || !req.body.password
+    ? res.status(400).json({
+        error: "email and password are required"
+      })
+    : next();
 }
 
 function validateUniqueEmail(req, res, next) {
-    users.findBy({
-            email: req.body.email
-        })
-        .then(user => {
-            // is there a user with this email? if not, next(). Else:
-            !user ? next() :
-                // does an id in the params exist, and does it equal the user's id?
-                // if true, next(). Else: the email is already in use
-                req.params.id && parseInt(req.params.id) === user.id ?
-                next() :
-                res.status(400).json({
-                    error: "That email is already in use."
-                })
-
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: "Couldn't check email uniqueness."
-            })
-        })
+  users
+    .findBy({
+      email: req.body.email
+    })
+    .then(user => {
+      // is there a user with this email? if not, next(). Else:
+      !user
+        ? next()
+        : // does an id in the params exist, and does it equal the user's id?
+        // if true, next(). Else: the email is already in use
+        req.params.id && parseInt(req.params.id) === user.id
+        ? next()
+        : res.status(400).json({
+            error: "That email is already in use."
+          });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "Couldn't check email uniqueness."
+      });
+    });
 }
 
 function validateUniqueStudentEmail(req, res, next) {
-    students.findBy({
-            email: req.body.email
-        })
-        .then(student => {
-            // is there a user with this email? if not, next(). Else:
-            !student ? next() :
-                // does an id in the params exist, and does it equal the student's id?
-                // if true, next(). Else: the email is already in use
-                req.params.id && parseInt(req.params.id) === student.id ?
-                next() :
-                res.status(400).json({
-                    error: "That email is already in use."
-                })
-
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: "Couldn't check email uniqueness."
-            })
-        })
+  students
+    .findBy({
+      email: req.body.email
+    })
+    .then(student => {
+      // is there a user with this email? if not, next(). Else:
+      !student
+        ? next()
+        : // does an id in the params exist, and does it equal the student's id?
+        // if true, next(). Else: the email is already in use
+        req.params.id && parseInt(req.params.id) === student.id
+        ? next()
+        : res.status(400).json({
+            error: "That email is already in use."
+          });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "Couldn't check email uniqueness."
+      });
+    });
 }
 
 function validateStudentId(req, res, next) {
-    students.findBy({
-            id: req.params.id
-        })
-        .then(student => {
-            if (student) {
-                req.student = student;
-                next();
-            } else {
-                res.status(400).json({
-                    error: "That student does not exist."
-                })
-            }
-
-        })
-        .catch(({
-            name,
-            code,
-            message,
-            stack
-        }) => {
-            res.status(500).json({
-                name,
-                code,
-                message,
-                stack
-            })
-        })
+  students
+    .findBy({
+      id: req.params.id
+    })
+    .then(student => {
+      if (student) {
+        req.student = student;
+        next();
+      } else {
+        res.status(400).json({
+          error: "That student does not exist."
+        });
+      }
+    })
+    .catch(({ name, code, message, stack }) => {
+      res.status(500).json({
+        name,
+        code,
+        message,
+        stack
+      });
+    });
 }
 
 function validateStudentBody(req, res, next) {
-    Object.keys(req.body).length === 0 && req.body.constructor === Object ?
-        res.status(400).json({
-            error: "Missing request body."
-        }) :
-        !req.body.firstName || !req.body.lastName || !req.body.email || !req.body.professor_Id || req.body.professor_Id !== parseInt(req.body.professor_Id) ?
-        res.status(400).json({
-            error: "firstName, lastName, email, and professor_Id are required. The professor_Id must be an integer."
-        }) :
-        next()
+  Object.keys(req.body).length === 0 && req.body.constructor === Object
+    ? res.status(400).json({
+        error: "Missing request body."
+      })
+    : !req.body.firstName ||
+      !req.body.lastName ||
+      !req.body.email ||
+      !req.body.professor_Id ||
+      req.body.professor_Id !== parseInt(req.body.professor_Id)
+    ? res.status(400).json({
+        error:
+          "firstName, lastName, email, and professor_Id are required. The professor_Id must be an integer."
+      })
+    : next();
+}
+
+function validateProjectBody(req, res, next) {
+  const { name, deadline } = req.body;
+  if (!name || !deadline) {
+    res.status(400).json({
+      error: "Please provide name and deadline for the project"
+    });
+  } else {
+    next();
+  }
 }
