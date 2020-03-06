757e5efb064fa19b485a37a71c3af015b10f71c9
Phong Yang <phongyang88@hotmail.com>
2020-03-05 20:31:24 -0600

    commenting out projectCategories seeds and moving to automaticaly be inserted into the projectCategories migration file. These preset categories will now be inserted into the database upon migration

--- database/seeds/003_project_categories.js
+++ database/seeds/003_project_categories.js
@@ -1,20 +1,24 @@
+// exports.seed = function (knex) {
+//   // Deletes ALL existing entries
+//   return knex('Project-Categories').del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('Project-Categories').insert([{
+//           name: 'Letter of recommendation'
+//         },
+//         {
+//           name: 'Referral'
+//         },
+//         {
+//           name: 'Help / Advice / Mentorship'
+//         },
+//         {
+//           name: "Other"
+//         }
+//       ]);
+//     });
+// };
+
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('Project-Categories').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('Project-Categories').insert([{
-          name: 'Letter of recommendation'
-        },
-        {
-          name: 'Referral'
-        },
-        {
-          name: 'Help / Advice / Mentorship'
-        },
-        {
-          name: "Other"
-        }
-      ]);
-    });
-};
+  return ''
+}
